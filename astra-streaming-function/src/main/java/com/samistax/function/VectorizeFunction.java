d9e371d539847db2730da46ce06c2a71c7acd887
samistax <sami.kaksonen@datastax.com>
2025-03-05 15:30:24 +0200

    Fixed compilation issue

--- astra-streaming-function/src/main/java/com/samistax/function/VectorizeFunction.java
+++ astra-streaming-function/src/main/java/com/samistax/function/VectorizeFunction.java
@@ -18,7 +18,7 @@ import java.util.Map;
 
 public class VectorizeFunction implements Function<GenericObject, Void> {
 
-    private static final Logger logger = LoggerFactory.getLogger(TestFunction.class);
+    private static final Logger logger = LoggerFactory.getLogger(Fi.class);
     private Collection<Document> collection;
     private com.fasterxml.jackson.databind.ObjectMapper pulsarObjectMapper = new com.fasterxml.jackson.databind.ObjectMapper(); // Unshaded Jackson ObjectMapper for Pulsar
     private String astraApiEndpoint;

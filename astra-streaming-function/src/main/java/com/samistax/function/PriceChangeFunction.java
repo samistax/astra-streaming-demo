2b782ad5fcc433cf3d137c0d72ac8037b2a68391
samistax <sami.kaksonen@datastax.com>
2025-03-06 08:52:07 +0200

    Updated input schema

--- astra-streaming-function/src/main/java/com/samistax/function/PriceChangeFunction.java
+++ astra-streaming-function/src/main/java/com/samistax/function/PriceChangeFunction.java
@@ -7,27 +7,28 @@ import org.apache.pulsar.client.api.Schema;
 import org.apache.pulsar.functions.api.Context;
 import org.apache.pulsar.functions.api.Function;
 
-public class PriceChangeFunction implements Function<String, PriceUpdateEvent> {
+public class PriceChangeFunction implements Function<PriceUpdateEvent, PriceUpdateEvent> {
 
     @Override
-    public PriceUpdateEvent process(String input, Context context) throws Exception {
+    public PriceUpdateEvent process(PriceUpdateEvent input, Context context) throws Exception {
 
         // Create an ObjectMapper instance for parsing JSON
-        ObjectMapper mapper = new ObjectMapper();
+       // ObjectMapper mapper = new ObjectMapper();
         // Parse the input JSON string to JsonNode
-        JsonNode jsonNode = mapper.readTree(input);
+        //JsonNode jsonNode = mapper.readTree(input);
         // Example how to map JSON to a POJO
-        PriceUpdateEvent se = mapper.readValue(input, PriceUpdateEvent.class);
-        int quantity = jsonNode.get("quantity").asInt();
+        //PriceUpdateEvent se = mapper.readValue(input, PriceUpdateEvent.class);
+        int quantity = input.getQuantity();
         // Using quantity as a dummy trigger for sinkign subset of input messages
         if ( quantity > 10 ) {
             // Create new output topic name
-            String outputTopicName = context.getInputTopics().stream().findFirst().get() + "-processed";
+            String outputTopicName = context.getInputTopics().stream().findFirst().get() + "-largedeals";
             // Publish new output message to the anomaly topic
-            context.newOutputMessage(outputTopicName, Schema.JSON(PriceUpdateEvent.class)).value(se).sendAsync();
+            input.setItemVariant("processed");
+            context.newOutputMessage(outputTopicName, Schema.JSON(PriceUpdateEvent.class)).value(input).sendAsync();
         }
         // Processed object will be sent to the topic configured as output when setting up the function in Astra Streaming
-        return se;
+        return input;
     }
 }
 
